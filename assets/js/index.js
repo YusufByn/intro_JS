f66e8bcf5e0d04a2f8eeaf4ecbd6ba62e09b486f
YusufByn <yusuf.buyukaydin33@gmail.com>
2025-07-18 15:46:16 +0200

    first-commit

---
+++ assets/js/index.js
@@ -0,0 +1,65 @@
+// alert("hello world from js file");
+
+// const phonePrice = 200;
+// const remise = 35;
+// const earBuds = 75;
+// const tva = 10;
+// const secondRemise = 20;
+// let total = 0;
+// // let pour le total car il va varier, étant donné que c'est le résultat de l'addtion des autres
+// // const pour les prix car ils ne bougent pas étant donné que ca va être le total qui va bouger
+
+// total = phonePrice + earBuds;
+
+// console.log(total)
+
+// total = total - remise;
+
+// console.log(total)
+
+// total = total - tva;
+
+// console.log(total)
+
+// total = total - secondRemise
+
+// console.log(total)
+
+
+// Exo1)
+
+const Nom = "Yusuf Buyukaydin";
+
+console.log(Nom)
+
+alert(Nom)
+
+// Exo2)
+
+let Age = 22;
+let Message = "Je suis " + Nom + " et j'ai " + Age;
+
+console.log(Message)
+
+// exo 3)
+
+let One = 1;
+let plusOneAge = 0
+
+plusOneAge = Age + One;
+
+console.log(plusOneAge)
+
+let SecondMessage = "L'année prochaine j'aurais " + plusOneAge;
+
+console.log(SecondMessage)
+
+// exo 4)
+
+let toto = "toto";
+let tata = "tata";
+let invers = tata;
+tata = toto;
+toto = invers;
+
+console.log("dans toto:" + toto, "dans tata:" + tata)
